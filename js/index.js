6c1b72f8f191c40df12bbe955f1dca142f414fd9
Christine Cua <cloudycua@gmail.com>
2019-02-26 17:16:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ main.addEventListener('click', function(event) {
 });
 
 const input = document.querySelector('input');
- 
+
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
 });
