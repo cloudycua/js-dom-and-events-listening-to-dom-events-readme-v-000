ea73f4ea436baa9ba7282b8f4dacae07fc8dd72f
Christine Cua <cloudycua@gmail.com>
2019-02-26 17:24:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -25,19 +25,36 @@ input.addEventListener('keydown', function(e) {
   }
 });
 
-let divs = document.querySelectorAll('div');
+// BUBBLED
+// target node is the first node to bubble the event up
 
-function bubble(e) {
-  // remember all of those fancy DOM node properties?
-  // we're making use of them to get the number
-  // in each div here!
+    let divs = document.querySelectorAll('div');
 
-  // if `this` is a bit confusing, don't worry —
-  // for now, know that it refers to the div that
-  // is triggering the current event handler.
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
-}
+    function bubble(e) {
+      // remember all of those fancy DOM node properties?
+      // we're making use of them to get the number
+      // in each div here!
 
-for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener('click', bubble);
-}
+      // if `this` is a bit confusing, don't worry —
+      // for now, know that it refers to the div that
+      // is triggering the current event handler.
+      console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+    }
+
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].addEventListener('click', bubble);
+    }
+
+// CAPTURED
+// target node is the last node to bubble the event up
+
+    divs = document.querySelectorAll('div');
+     
+    function capture(e) {
+      console.log(this.firstChild.nodeValue.trim() + ' captured');
+    }
+     
+    for (let i = 0; i < divs.length; i++) {
+      // set the third argument to `true`!
+      divs[i].addEventListener('click', capture, true);
+    }
