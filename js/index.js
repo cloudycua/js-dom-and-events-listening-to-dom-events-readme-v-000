d2e77f690732382fc17ac4a3dee12464208b1d2d
Christine Cua <cloudycua@gmail.com>
2019-02-26 17:25:06 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -58,3 +58,20 @@ input.addEventListener('keydown', function(e) {
       // set the third argument to `true`!
       divs[i].addEventListener('click', capture, true);
     }
+
+
+// STOP PROPOGATION
+
+    const divs = document.querySelectorAll('div');
+
+    function bubble(e) {
+    // stop! that! propagation!
+    e.stopPropagation();
+
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+    }
+
+    for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+    }
+    
